723b69a32a3a1502db0fc647d697e937fe726118
Asonna <Asonna>
2017-02-18 06:08:31 -0800

    set up and activate the homepage; add picture in homepage

--- src/SonOfCodSeafood/Startup.cs
+++ src/SonOfCodSeafood/Startup.cs
@@ -57,7 +57,7 @@ namespace SonOfCodSeafood
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=FlickrLike}/{action=Index}/{id?}");
+                    template: "{controller=Home}/{action=Index}/{id?}");
             });
 
             app.Run(async (context) =>
